e5b71fb70deba3d2e72f71fc610a6e17c7f8c712
Pixel <99288538+Pixel2075@users.noreply.github.com>
2023-01-14 18:34:54 +0500

    leaked token

--- config/config.py
+++ config/config.py
@@ -1,9 +1,9 @@
-BOT_TOKEN = "MTA0NTA3MTI1Njc5NTQ4MDE4Ng.Gs337C.mi86zTWScNqJ7gmP73I5qC0KeHRWMK2Pnvoqeg"
+BOT_TOKEN = ""
 BOT_PREFIX = '>'
 STARTUP_COMPLETE_MESSAGE = 'Logged in as {botName}'
 STATUS_BOT = 'Help {any}'
 
-TOKEN = "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"
+TOKEN = ""
 URL = "https://api.openai.com/v1/models"
 
 DELETE_TIME = 5
